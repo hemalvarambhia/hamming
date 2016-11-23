e6f7db72bb653101e2a9b4fbae7acbb5095bbea4
Hemal <h.n.varambhia@gmail.com>
2016-11-23 13:02:08 +0000

    No need for the return keyword.

--- hamming.rb
+++ hamming.rb
@@ -6,7 +6,7 @@ class Hamming
   def self.compute seq_1, seq_2
     raise ArgumentError if seq_1.size != seq_2.size
 
-    return seq_1.chars.zip(seq_2.chars).count do |nucleo_1, nucleo_2|
+    seq_1.chars.zip(seq_2.chars).count do |nucleo_1, nucleo_2|
       nucleo_1 != nucleo_2
     end
   end
