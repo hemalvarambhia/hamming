a725520f96e82d2cff3cd82810adba38f1da90c0
Hemal <h.n.varambhia@gmail.com>
2016-11-23 13:10:32 +0000

    Added a helpful error message.

--- hamming.rb
+++ hamming.rb
@@ -4,7 +4,14 @@ end
 
 class Hamming
   def self.compute seq_1, seq_2
-    raise ArgumentError if seq_1.size != seq_2.size
+    if seq_1.size != seq_2.size
+      raise ArgumentError.new(
+        %{
+          The sequences are not the same size:
+          first sequence is #{seq_1.size}, the second is #{seq_2.size}" 
+         }
+      )
+    end
 
     seq_1.chars.zip(seq_2.chars).count do |nucleo_1, nucleo_2|
       nucleo_1 != nucleo_2
